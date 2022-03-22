bb9f6345994e8bc752b08652cf55d129d8100018
ccozad <ccozad@gmail.com>
2022-03-21 22:06:35 -0700

    ledger_page additions
    - Fields exposed as read only
    - Implement from_str
    - Tests for fields
    - Tests for to_string and from_str

--- src/ledger_page.rs
+++ src/ledger_page.rs
@@ -1,6 +1,7 @@
 extern crate serde;
 
 use std::fmt;
+use std::str::FromStr;
 use crate::block::Block;
 use serde::{Serialize, Deserialize};
 
@@ -10,8 +11,7 @@ pub struct LedgerPage {
     /// Storage for blocks
     entries: Vec<Block>,
     first_hash: String,
-    last_hash: Option<String>,
-    finalized: bool
+    last_hash: Option<String>
 }
 
 impl LedgerPage {
@@ -27,24 +27,21 @@ impl LedgerPage {
         LedgerPage {
             entries,
             first_hash: String::from(initial_parent_hash),
-            last_hash: None,
-            finalized: false
+            last_hash: None
         }
     }
 
-    /// Adds a transaction to the ledger if it hasn't been finalized
+    /// Adds a transaction to the ledger
     pub fn add_transaction(&mut self, transactions: &str) {
-        if !self.finalized {
-            if self.entries.len() > 0 {
-                let parent_hash = self.entries.last().unwrap().hash();
-                let block = Block::new(parent_hash, transactions);
-                self.last_hash = Some(String::from(block.hash()));
-                self.entries.push(block);
-            } else {
-                let block = Block::new(self.first_hash.as_str(), transactions);
-                self.last_hash = Some(String::from(block.hash()));
-                self.entries.push(block);
-            }
+        if self.entries.len() > 0 {
+            let parent_hash = self.entries.last().unwrap().hash();
+            let block = Block::new(parent_hash, transactions);
+            self.last_hash = Some(String::from(block.hash()));
+            self.entries.push(block);
+        } else {
+            let block = Block::new(self.first_hash.as_str(), transactions);
+            self.last_hash = Some(String::from(block.hash()));
+            self.entries.push(block);
         }
     }
 
@@ -65,12 +62,37 @@ impl LedgerPage {
 
         is_verified
     }
+
+    pub fn first_hash(&self) -> &str {
+        self.first_hash.as_str()
+    }
+
+    pub fn last_hash(&self) -> Option<&str> {
+        if self.last_hash.is_some() {
+            let hash = self.last_hash.as_ref().unwrap().as_str();
+            Some(hash)
+        } else {
+            None
+        }
+    }
+
+    pub fn entries(&self) -> &[Block] {
+        &self.entries[..]
+    }
 }
 
 impl fmt::Display for LedgerPage {
     /// Represents the block as a JSON object string
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_str(&serde_json::to_string(self).unwrap())
+    }
+}
+
+impl FromStr for LedgerPage {
+    type Err = serde_json::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        serde_json::from_str(s)
     }
 }
 
@@ -82,12 +104,43 @@ fn test_constructor() {
 }
 
 #[test]
+fn test_first_hash() {
+    let mut lp = LedgerPage::new("MA==");
+    lp.add_transaction("Hello World");
+
+    assert_eq!("MA==", lp.first_hash());
+}
+
+#[test]
+fn test_last_hash() {
+    let mut lp = LedgerPage::new("MA==");
+    lp.add_transaction("Hello World");
+
+    assert_eq!("RGUWhlfUKobrBmf5xjKPHUCBVe2wuP+FbDrLfQXEz2g=", lp.last_hash().unwrap());
+}
+
+#[test]
+fn test_entries() {
+    let mut lp = LedgerPage::new("MA==");
+    lp.add_transaction("Hello World");
+
+    let entries = lp.entries();
+
+    assert_eq!(1, entries.len());
+    assert_eq!("MA==", entries[0].parent_hash());
+    assert_eq!("RGUWhlfUKobrBmf5xjKPHUCBVe2wuP+FbDrLfQXEz2g=", entries[0].hash());
+    assert_eq!("Hello World", entries[0].transactions());
+    assert_eq!(true, entries[0].verify())
+}
+
+#[test]
 fn test_add_initial_transaction() {
     let mut lp = LedgerPage::new("MA==");
     lp.add_transaction("Hello World");
 
     assert_eq!(1, lp.entries.len());
     assert_eq!("RGUWhlfUKobrBmf5xjKPHUCBVe2wuP+FbDrLfQXEz2g=", lp.last_hash.unwrap());
+    assert_eq!("MA==", lp.first_hash);
 }
 
 #[test]
@@ -98,6 +151,7 @@ fn test_add_many_transactions() {
 
     assert_eq!(2, lp.entries.len());
     assert_eq!("Ga5LMtyBWQuto1w4WWAttuHQMrHn2N5YqJ1T7f+INJ4=", lp.last_hash.unwrap());
+    assert_eq!("MA==", lp.first_hash);
 }
 
 #[test]
@@ -110,4 +164,38 @@ fn test_verify_true() {
 
     assert_eq!(2, lp.entries.len());
     assert_eq!(true, lp.verify());
+}
+
+#[test]
+fn test_to_string() {
+    let mut lp = LedgerPage::new("MA==");
+    lp.add_transaction("Hello World");
+
+    let lp_serialized = lp.to_string();
+    let expected_result = r#"{"entries":[{"parent_hash":"MA==","transactions":"Hello World","hash":"RGUWhlfUKobrBmf5xjKPHUCBVe2wuP+FbDrLfQXEz2g="}],"first_hash":"MA==","last_hash":"RGUWhlfUKobrBmf5xjKPHUCBVe2wuP+FbDrLfQXEz2g="}"#;
+
+    assert_eq!(expected_result, lp_serialized);
+}
+
+#[test]
+fn test_from_str() {
+    let lp_serialized = r#"{"entries":[{"parent_hash":"MA==","transactions":"Hello World","hash":"RGUWhlfUKobrBmf5xjKPHUCBVe2wuP+FbDrLfQXEz2g="}],"first_hash":"MA==","last_hash":"RGUWhlfUKobrBmf5xjKPHUCBVe2wuP+FbDrLfQXEz2g="}"#;
+    let lp = LedgerPage::from_str(&lp_serialized).unwrap();
+
+    assert_eq!(1, lp.entries.len());
+    assert_eq!("RGUWhlfUKobrBmf5xjKPHUCBVe2wuP+FbDrLfQXEz2g=", lp.last_hash.unwrap());
+    assert_eq!("MA==", lp.first_hash);
+}
+
+#[test]
+fn test_verify_false() {
+    let lp_serialized = r#"{"entries":[{"parent_hash":"MA==","transactions":"Hello World","hash":"AGUWhlfUKobrBmf5xjKPHUCBVe2wuP+FbDrLfQXEz2g="}],"first_hash":"MA==","last_hash":"RGUWhlfUKobrBmf5xjKPHUCBVe2wuP+FbDrLfQXEz2g="}"#;
+    let lp = LedgerPage::from_str(&lp_serialized).unwrap();
+
+    assert_eq!(false, lp.verify());
+
+    let lp_serialized2 = r#"{"entries":[{"parent_hash":"MA==","transactions":"Hello World","hash":"RGUWhlfUKobrBmf5xjKPHUCBVe2wuP+FbDrLfQXEz2g="}],"first_hash":"GA==","last_hash":"RGUWhlfUKobrBmf5xjKPHUCBVe2wuP+FbDrLfQXEz2g="}"#;
+    let lp2 = LedgerPage::from_str(&lp_serialized2).unwrap();
+
+    assert_eq!(false, lp2.verify());
 }
